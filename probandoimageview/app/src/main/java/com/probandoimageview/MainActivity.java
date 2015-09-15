c405a909bbf557daff99d8cb4ae24bc7b30e59a5
Felix Ziegler <felix.uni@gmail.com>
2015-09-15 14:12:57 -0430

    first commit

---
+++ probandoimageview/app/src/main/java/com/probandoimageview/MainActivity.java
@@ -0,0 +1,62 @@
+package com.probandoimageview;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.pkmmte.view.CircularImageView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CircularImageView foto=(CircularImageView)findViewById(R.id.fotico);
+        foto.setImageResource(R.drawable.intro4);
+        foto.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                startActivity(new Intent(MainActivity.this,MapsActivity.class));
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
